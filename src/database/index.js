d271f44f61be52915f468f04840aaa0727ef9be6
Ax de Sousa <axsilvasousa@gmai.com>
2019-10-17 14:25:51 -0300

    import models

--- src/database/index.js
+++ src/database/index.js
@@ -1,18 +1,29 @@
-const Sequelize = require('sequelize');
-const dbConfig = require('../config/database');
+const fs = require("fs")
+const path = require("path")
+const basename = path.basename(__filename)
+const Sequelize = require("sequelize")
+const dbConfig = require("../config/database")
 
-const User = require('../models/User');
-const Address = require('../models/Address');
-const Tech = require('../models/Tech');
+const connection = new Sequelize(dbConfig)
 
-const connection = new Sequelize(dbConfig);
+const models = []
 
-User.init(connection);
-Address.init(connection);
-Tech.init(connection);
+fs.readdirSync(__dirname)
+    .filter(file => {
+        return file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    })
+    .forEach(file => {
+        models.push(require(file))
+    })
 
-User.associate(connection.models);
-Address.associate(connection.models);
-Tech.associate(connection.models);
+models.forEach(model => {
+    model.init(connection)
+})
 
-module.exports = connection;
+Object.keys(connection.models).forEach(modelName => {
+    if (connection.models[modelName].associate) {
+        connection.models[modelName].associate(connection.models)
+    }
+})
+
+module.exports = connection
